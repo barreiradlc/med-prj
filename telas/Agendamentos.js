3b0459898b918942c30d9dc7d3e9daa8f43657e9
Matheus Michelis <basconian.michelis@gmail.com>
2019-11-09 15:34:53 -0300

    Update Agendamentos.js
    Alimentação do agendamento

--- telas/Agendamentos.js
+++ telas/Agendamentos.js
@@ -10,7 +10,7 @@ function Agendamentos(){
 const list = [
   {
     name: 'Amy Farha',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    avatar_url: 'https://i0.wp.com/www.hisour.com/wp-content/uploads/2018/03/Alabama-crimson-color.jpg?fit=720%2C720&ssl=1',
     subtitle: 'Vice President'
   },
   {
@@ -27,7 +27,7 @@ const list = [
         list.map((l, i) => (
           <ListItem
             key={i}
-            leftIcon={{ name: i.icon }}
+            leftAvatar={{ source: { uri: l.avatar_url } }}
             title={l.name}
             subtitle={l.subtitle}
             bottomDivider
